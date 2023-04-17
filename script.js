35344bf8228cbd3cbfc516fee2328d80124943ec
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2023-04-17 10:29:17 +0200

    import and convert data

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 "use strict";
+// variables
+const myData = "https://testdaba-a7b1d-default-rtdb.europe-west1.firebasedatabase.app";
+window.addEventListener("load", start);
 
-"do a script";
+async function start() {
+  console.log(await getPosts());
+}
+
+async function getPosts() {
+  const rawData = await fetch(myData + "/posts.json");
+  const data = await rawData.json();
+  preparePostsData(data);
+  return data;
+}
+
+function preparePostsData(dataObject) {
+  const postArray = [];
+  for (const key in dataObject) {
+    const post = dataObject[key];
+    postArray.push(post);
+  }
+  console.log(postArray);
+  return postArray;
+}
