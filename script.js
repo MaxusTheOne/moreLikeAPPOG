0bb225251c02cb381d902e2baf23c93cf8902978
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2023-04-17 10:58:38 +0200

    function for displaying posts

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const myData = "https://testdaba-a7b1d-default-rtdb.europe-west1.firebasedatabas
 window.addEventListener("load", start);
 
 async function start() {
-  console.log(await getPosts());
+  const data = await getPosts();
+  for (const obj in data) displayPosts(data[obj]);
 }
 
 async function getPosts() {
   const rawData = await fetch(myData + "/posts.json");
   const data = await rawData.json();
-  preparePostsData(data);
-  return data;
+  return preparePostsData(data);
 }
 
 function preparePostsData(dataObject) {
@@ -20,6 +20,16 @@ function preparePostsData(dataObject) {
     const post = dataObject[key];
     postArray.push(post);
   }
-  console.log(postArray);
   return postArray;
+}
+
+function displayPosts(element) {
+  const postsSelector = document.querySelector("#posts");
+  console.log(`data give: ${element}`);
+  const htmlObj = /*HTML*/ `
+    <div class="post">
+        <img src="${element.image}" alt="">
+    </div>
+    `;
+  postsSelector.insertAdjacentHTML("beforeend", htmlObj);
 }
