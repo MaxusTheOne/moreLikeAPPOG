c9b7395207a87a5587e8868f2e92b3b5467554e3
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2023-04-18 15:34:24 +0200

    Added user funcs

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const myData = "https://testdaba-a7b1d-default-rtdb.europe-west1.firebasedatabas
 window.addEventListener("load", start);
 
 async function start() {
-  const data = await getPosts();
-  displayPosts(data);
-  displayUsers(data);
+  const postData = await getPosts();
+  const userData = await getUsers();
+  displayPosts(postData);
+  displayUsers(userData);
   // await createPost("waaaat", "https://images.unsplash.com/photo-1465779171454-aa85ccf23be6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bG9vcHN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60", "This is a thing");
-
   // updatePost("-NTJ2b6NrXk-E8yJyRO2", "My Second Post", "https://images.unsplash.com/photo-1641876749963-550554c7258d");
   // deletePost("-NTJ2b6NrXk-E8yJyRO2");
 }
@@ -27,6 +27,21 @@ function preparePostsData(dataObject) {
   }
   return postArray;
 }
+
+async function getUsers() {
+  const rawData = await fetch(myData + "/users.json");
+  const data = await rawData.json();
+  return prepareUsersData(data);
+}
+function prepareUsersData(dataObject) {
+  const userArray = [];
+  for (const key in dataObject) {
+    const user = dataObject[key];
+    userArray.push(user);
+  }
+  return userArray;
+}
+
 function displayPosts(objList) {
   for (const obj in objList) displayPost(objList[obj]);
 }
@@ -77,4 +92,23 @@ async function deletePost(id) {
   const url = `${myData}/posts/${id}.json`;
   const res = await fetch(url, { method: "DELETE" });
   console.log(res);
+}
+
+function displayUsers(objList) {
+  for (const obj in objList) displayUser(objList[obj]);
+}
+
+function displayUser(element) {
+  const usersSelector = document.querySelector("#users");
+  console.log(`data given: ${element}`);
+  const htmlObj = /*HTML*/ `
+    <div class="user">
+        <h3>${element.title}</h3>
+        <h3>${element.name}</h3>
+        <h4>${element.mail}</h4>
+        <img src="${element.image}" alt="">
+        <div class="body">${element.phone}</div>
+    </div>
+    `;
+  usersSelector.insertAdjacentHTML("beforeend", htmlObj);
 }
