a6d52b2223ad268bcd253b4610f577d0282c3835
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2023-04-18 13:29:09 +0200

    Styling and create new post func

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ window.addEventListener("load", start);
 async function start() {
   const data = await getPosts();
   for (const obj in data) displayPosts(data[obj]);
+  // await createPost("waaaat", "https://images.unsplash.com/photo-1465779171454-aa85ccf23be6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bG9vcHN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60", "This is a thing");
 }
 
 async function getPosts() {
@@ -25,11 +26,26 @@ function preparePostsData(dataObject) {
 
 function displayPosts(element) {
   const postsSelector = document.querySelector("#posts");
-  console.log(`data give: ${element}`);
+  console.log(`data given: ${element}`);
   const htmlObj = /*HTML*/ `
     <div class="post">
+        <h3>${element.title}</h3>
         <img src="${element.image}" alt="">
     </div>
     `;
   postsSelector.insertAdjacentHTML("beforeend", htmlObj);
+}
+async function createPost(title, image, body) {
+  const postObj = {
+    title: `${title}`,
+    image: `${image}`,
+    body: `${body}`,
+  };
+  console.log(postObj);
+  const postToJson = JSON.stringify(postObj);
+  console.log(postToJson);
+  const response = await fetch(`${myData}/posts.json`, {
+    method: "POST",
+    body: postToJson,
+  });
 }
